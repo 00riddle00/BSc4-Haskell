1aac9d09662f6fc2d1c9a2d8c68f4862119b4a52
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2021-11-06 18:21:31 +0200

    Update lab3 ex1

--- lab_3.hs
+++ lab_3.hs
@@ -28,16 +28,42 @@ doOverlap (l1x,l1y) (r1x,r1y) (l2x,l2y) (r2x,r2y)
     | l1x >= r2x || l2x >= r1x = False
     | r1y >= l2y || r2y >= l1y = False
     | otherwise = True
-       
+
+coverlap :: Shape -> Shape -> Bool
+coverlap (Circle r1 (x1,y1)) (Circle r2 (x2,y2))
+    | (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) < (r1 + r2) * (r1 + r2) = True
+    | otherwise = False
+
+rcoverlap :: Shape -> Shape -> Bool
+rcoverlap (Rectangle h w (x,y)) (Circle r (xc,yc))
+    | (dx**2 + dy**2) < r**2 = True
+    | otherwise = False
+        where 
+            dx = xn - xc
+            dy = yn - yc
+            xn = max x1 (min xc x2)
+            yn = max y2 (min yc y1)
+            (x1, y1) = (topLeft (Rectangle h w (x,y)))
+            (x2, y2) = (bottomRight (Rectangle h w (x,y)))
+rcoverlap (Circle r (xc,yc)) (Rectangle h w (x,y)) = 
+    rcoverlap (Rectangle h w (x,y)) (Circle r (xc,yc)) 
 
 {-
-overlap (Rectangle 10 10 (5,5)) (Rectangle 5 10 (10, 2.5)) - True
-l1 = (0, 10)
-r1 = (10, 0)
+overlap (Rectangle 1 1 (1,1)) (Rectangle 1 1 (1,1)) - True
+overlap (Rectangle 10 10 (5,5)) (Rectangle 5 10 (10,2.5)) - True
+-- Do not overlap, just touch each other at the corner:
+overlap (Rectangle 2 2 (3,1)) (Rectangle 2 2 (1,3)) - False
+-- Do not overlap, just touch each other at the side:
+overlap (Rectangle 2 2 (3,1)) (Rectangle 2 2 (1,1)) - False
 
-l2 = (5, 5)
-r2 = (15, 0)
+coverlap (Circle 1 (1,1)) (Circle 1 (1,1)) - True
+-- Do not overlap, just touch each other:
+coverlap (Circle 30 ((-10),8)) (Circle 10 (14,(-24))) - False
 
-overlap (Circle 1 (5,4)) (Rectangle 4 10 (6,1)) - True
-overlap (Circle 1 (1,1)) (Circle 1 (1,1)) - True
+rcoverlap (Rectangle 3 2 (2,1.5)) (Circle 2 (0,0)) - True
+rcoverlap (Circle 2 (5,4)) (Rectangle 4 10 (6,1)) - True
+rcoverlap (Rectangle 3 2 (4,4.5)) (Circle 1 (1,1)) - False
+-- Do not overlap, just touch each other:
+rcoverlap (Rectangle 2 2 (1,1)) (Circle 1 (3,1)) - False
+rcoverlap (Rectangle 2 2 (2,0)) (Circle 1 (0,0)) - False
 -}
