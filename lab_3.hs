62f3a2dcdd9952fc7e519e4749e570304f9c787c
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2021-11-08 13:41:21 +0200

    First attempt at lab3 ex8

--- lab_3.hs
+++ lab_3.hs
@@ -99,3 +99,20 @@ iter1 n f
 iter2 :: Integer -> (a -> a) -> a -> a
 iter2 n f = foldr (.) id (replicate (fromIntegral(n)) f)
 
+-- ----------------------------------------------
+-- Exercise 8
+-- ----------------------------------------------
+
+-- which returns all the ways that a list can be split into two consecutive ones
+--splits1 :: [a] -> [([a],[a])]
+--splits1 str =
+    --[ ( (take 0 str), (drop 0 str) ), ( (take 1 str), (drop 1 str) ), ( (take 2 str), (drop 2 str) ), ( (take 3 str), (drop 3 str) ), ( (take 4 str), (drop 4 str) ) ]
+
+splits :: [a] -> [([a],[a])]
+splits str = splits' str str 0
+    where
+        splits' :: [a] -> [a] -> Int -> [([a],[a])]
+        splits' str [] _ = []
+        splits' str _ 0 = [ ( (take 0 str), (drop 0 str) ) ] ++ (splits' str str 1)
+        splits' str (x:xs) n = [ ( (take n str), (drop n str) ) ] ++ (splits' str xs (n+1))
+
